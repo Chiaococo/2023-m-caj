e8bbc085c5c6e10c753039bfae9aee56d21a948a
Chiao <87346275+Chiaococo@users.noreply.github.com>
2023-10-31 15:33:19 +0800

    Update PassengerTypeRepositoryImpl.java

--- masterfile/src/main/java/com/caj/infra/repository/PassengerTypeRepositoryImpl.java
+++ masterfile/src/main/java/com/caj/infra/repository/PassengerTypeRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.caj.infra.repository;
 
+import java.util.Optional;
+import com.caj.infra.repository.po.PassengerTypePo;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,9 +40,13 @@ public class PassengerTypeRepositoryImpl implements PassengerTypeRepository {
 	 * 取得折扣率
 	 */
 	@Override
-	public BigDecimal findPassengerDiscount(String passengerType) {
-		return 	passengerTypeDao.findById(passengerType).get().getDiscount();
-	}
+        public BigDecimal findPassengerDiscount(String passengerType) {
+         Optional<PassengerTypePo> result = passengerTypeDao.findById(passengerType);
+         if (result.isPresent()) {
+          return result.get().getDiscount();
+         };
+         return null;
+        }
 
 
 }
